4fb489b18aec2ecae575614e711185178730a8c8
Manh Tuan <buimanhtuan2k4@gmail.com>
2024-05-17 16:59:50 +0700

    commit

--- BaiTapThucHanh/B2.2.cpp
+++ BaiTapThucHanh/B2.2.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-// Hàm nhập vào dãy số nguyên
 void nhapDaySo(int arr[], int &n)
 {
     cout << "Nhập số lượng phần tử trong dãy: ";
@@ -14,7 +13,6 @@ void nhapDaySo(int arr[], int &n)
     }
 }
 
-// Hàm hiển thị dãy số nguyên
 void hienThiDaySo(int arr[], int n)
 {
     cout << "Dãy số: ";
@@ -25,7 +23,6 @@ void hienThiDaySo(int arr[], int n)
     cout << endl;
 }
 
-// Hàm tính tổng các phần tử trong dãy số
 int tinhTong(int arr[], int n)
 {
     int tong = 0;
@@ -36,7 +33,6 @@ int tinhTong(int arr[], int n)
     return tong;
 }
 
-// Hàm tìm giá trị lớn nhất trong dãy số
 int timGiaTriLonNhat(int arr[], int n)
 {
     int max = arr[0];
@@ -50,7 +46,6 @@ int timGiaTriLonNhat(int arr[], int n)
     return max;
 }
 
-// Hàm sắp xếp dãy số theo chiều tăng dần
 void sapXepDaySo(int arr[], int n)
 {
     for (int i = 0; i < n - 1; ++i)
@@ -69,30 +64,23 @@ void sapXepDaySo(int arr[], int n)
 
 int main()
 {
-    const int MAX_SIZE = 100; // Kích thước tối đa của mảng
+    const int MAX_SIZE = 100;
     int arr[MAX_SIZE];
     int n;
 
-    // Nhập dãy số
     nhapDaySo(arr, n);
 
-    // Hiển thị dãy số
     hienThiDaySo(arr, n);
 
-    // Tính tổng các phần tử trong dãy số
     int tong = tinhTong(arr, n);
     cout << "Tổng các phần tử trong dãy số: " << tong << endl;
 
-    // Tìm giá trị lớn nhất trong dãy số
     int giaTriLonNhat = timGiaTriLonNhat(arr, n);
     cout << "Giá trị lớn nhất trong dãy số: " << giaTriLonNhat << endl;
 
-    // Sắp xếp dãy số theo chiều tăng dần
     sapXepDaySo(arr, n);
     cout << "Dãy số sau khi sắp xếp theo chiều tăng dần: ";
     hienThiDaySo(arr, n);
 
     return 0;
 }
-
-// Theo ChaGPT
--- BaiTapThucHanh/B2.3.cpp
+++ BaiTapThucHanh/B2.3.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-// Hàm nhập vào mảng số nguyên và trả về tham chiếu tới mảng đó
 int (&nhapMang(int (&arr)[100], int &n))[100]
 {
     cout << "Nhập số lượng phần tử trong mảng: ";
@@ -15,7 +14,6 @@ int (&nhapMang(int (&arr)[100], int &n))[100]
     return arr;
 }
 
-// Hàm hiển thị mảng số nguyên
 void hienThiMang(const int arr[], int n)
 {
     cout << "Mảng số: ";
@@ -26,7 +24,6 @@ void hienThiMang(const int arr[], int n)
     cout << endl;
 }
 
-// Hàm sắp xếp mảng theo chiều tăng dần
 void sapXepMang(int arr[], int n)
 {
     for (int i = 0; i < n - 1; ++i)
@@ -49,17 +46,13 @@ int main()
     int arr[MAX_SIZE];
     int n;
 
-    // Nhập mảng
     int(&refArr)[100] = nhapMang(arr, n);
 
-    // Hiển thị mảng
     hienThiMang(refArr, n);
 
-    // Sắp xếp mảng theo chiều tăng dần
     sapXepMang(refArr, n);
     cout << "Mảng sau khi sắp xếp theo chiều tăng dần: ";
     hienThiMang(refArr, n);
 
     return 0;
 }
-// Theo ChaGPT
